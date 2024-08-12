34e6be52fa7c40494f38e890b970cb099779c658
Arjun <arjunmohanvermaamv@gmail.com>
2024-08-12 08:28:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Home from "./pages/Home/Home"
+// import Navbar from "./pages/Navbar"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact/Contact"
+import About from "./pages/About"
+import Blogs from "./pages/Blogs"
+import Footer from "./pages/Footer"
+import ShopProducts from"./pages/ShopProducts"
+import { Route, Routes } from "react-router-dom"
+import Nav from "./pages/Nav"
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Nav/>
+        <Routes>
+          <Route index path="/" element={<Home />} /> 
+          <Route path="/home" element={<Home/>}/> 
+          <Route path="/home" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blogs" element={<Blogs />} /> 
+          <Route path="/ShopProducts" element={<ShopProducts />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
+
